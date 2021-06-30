33c6c4535d76e6470c4034225fea314cb57d9521
Sayar1106 <sayarbanerjee@gmail.com>
2021-06-30 23:12:58 +0530

    Formatting

--- agents/autoencoder.py
+++ agents/autoencoder.py
@@ -3,18 +3,21 @@ from torch.nn import functional as F
 from torch.nn.modules.conv import Conv2d
 from torch.optim import Adam
 
+
 class AutoEncoder(nn.Module):
-    def __init__(self
-    , input_dim
-    , encoder_conv_filters
-    , encoder_conv_kernel_size
-    , encoder_conv_strides
-    , decoder_conv_t_filters
-    , decoder_conv_t_kernel_size
-    , decoder_conv_t_strides
-    , z_dim
-    , use_batch_norm=False
-    , use_dropout=False):
+    def __init__(
+        self,
+        input_dim,
+        encoder_conv_filters,
+        encoder_conv_kernel_size,
+        encoder_conv_strides,
+        decoder_conv_t_filters,
+        decoder_conv_t_kernel_size,
+        decoder_conv_t_strides,
+        z_dim,
+        use_batch_norm=False,
+        use_dropout=False,
+    ):
 
         super(AutoEncoder, self).__init__()
         self.input_dim = input_dim
@@ -33,17 +36,18 @@ class AutoEncoder(nn.Module):
             if i == 0:
                 input_channel = self.input_dim[-1]
             else:
-                input_channel=self.encoder_conv_filters[-1]
+                input_channel = self.encoder_conv_filters[-1]
             self.conv_layers.append(
-                nn.Conv2d(input_channel,
-                          out_channels=self.encoder_conv_filters[i],
-                          kernel_size=self.encoder_conv_kernel_size,
-                          stride=self.encoder_conv_strides
+                nn.Conv2d(
+                    input_channel,
+                    out_channels=self.encoder_conv_filters[i],
+                    kernel_size=self.encoder_conv_kernel_size,
+                    stride=self.encoder_conv_strides,
                 )
             )
-        
+
         self.output_layer = nn.Linear(self.encoder_conv_filters[-1], self.z_dim)
-    
+
     def forward(self, features):
         x = None
         for i in range(len(self.conv_layers)):
@@ -54,15 +58,16 @@ class AutoEncoder(nn.Module):
         return x
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = AutoEncoder(
-input_dim = (28,28,1)
-, encoder_conv_filters = [32,64,64, 64]
-, encoder_conv_kernel_size = [3,3,3,3]
-, encoder_conv_strides = [1,2,2,1]
-, decoder_conv_t_filters = [64,64,32,1]
-, decoder_conv_t_kernel_size = [3,3,3,3]
-, decoder_conv_t_strides = [1,2,2,1]
-, z_dim = 2)
+        input_dim=(28, 28, 1),
+        encoder_conv_filters=[32, 64, 64, 64],
+        encoder_conv_kernel_size=[3, 3, 3, 3],
+        encoder_conv_strides=[1, 2, 2, 1],
+        decoder_conv_t_filters=[64, 64, 32, 1],
+        decoder_conv_t_kernel_size=[3, 3, 3, 3],
+        decoder_conv_t_strides=[1, 2, 2, 1],
+        z_dim=2,
+    )
 
     print(model)
